d037f7dd2ed06eaa155d383aaaf331e941fb8181
Finnley Grange <dev.finnleygrange@icloud.com>
2024-02-06 01:46:49 +0000

    requesting artist data

--- index.js
+++ index.js
@@ -6,29 +6,45 @@ const port = 3000;
 const clientId = "c1be18ff1a82495783d9cfefdd503533";
 const clientSecret = "9f33969bfc274ec6bc19c1ac5c2d7a91";
 
+let access_token;
+
 const authOptions = {
   url: "https://accounts.spotify.com/api/token",
   method: "post",
   headers: {
-    Authorization: `Basic ${Buffer.from(clientId + ":" + clientSecret).toString(
-      "base64"
-    )}`,
+    Authorization:
+      "Basic " +
+      new Buffer.from(clientId + ":" + clientSecret).toString("base64"),
     "Content-Type": "application/x-www-form-urlencoded",
   },
   params: {
     grant_type: "client_credentials",
   },
+  json: true,
 };
 
-axios(authOptions).then((res) => {
-  console.log(res.data.access_token);
-});
+const artistsId = "5Mhs3Eu8lU6sRCtRYsmABV";
 
-app.post(authOptions, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    var token = body.access_token;
-  }
-});
+const artistOptions = {
+  url: `https://api.spotify.com/v1/artists/${artistsId}`,
+  method: "get",
+  headers: {
+    Authorization: "",
+  },
+};
+
+axios(authOptions)
+  .then((res) => {
+    access_token = res.data.access_token;
+    artistOptions.headers.Authorization = `Bearer ${access_token}`;
+    return axios(artistOptions);
+  })
+  .then((res) => {
+    console.log(res.data);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
